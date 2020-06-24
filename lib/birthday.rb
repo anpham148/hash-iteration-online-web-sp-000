58b9e315d4fa252343da90eeeef13d3b0268b349
An Pham <anpham148@gmail.com>
2020-06-24 11:10:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,6 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each {|kids_name, age| age < 12 ? puts "Happy Birthday #{kids_name}! You are now #{age} years old!" : "You ar
-e too old for this."} 
+e too old for this."}
     
 end
